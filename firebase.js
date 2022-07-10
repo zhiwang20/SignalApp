f89f8e2f5601cd6325a3367aa74376a39cadaf0a
Zhiqiang Wang <zhiwang20@yahoo.com>
2022-07-09 21:36:04 -0400

    first commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCilRm9bvnKOTn690RFeQl_fj55xr9okQw",
+  authDomain: "signalapp-73a63.firebaseapp.com",
+  projectId: "signalapp-73a63",
+  storageBucket: "signalapp-73a63.appspot.com",
+  messagingSenderId: "507688199032",
+  appId: "1:507688199032:web:0ef4fc51f053a967eb5e8d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
